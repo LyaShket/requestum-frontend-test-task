ca6f765c9afcb7668294e941ae89185e6cd9cdd1
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-06-23 14:28:50 +0300

    remove test rows

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 import {CurveTool} from './curve-tool';
 
-const images = document.querySelectorAll('img');
-images.forEach(image => {
-  image.src = new URL(image.src, import.meta.url).toString();
-});
-
 const curveTool = new CurveTool('container');
